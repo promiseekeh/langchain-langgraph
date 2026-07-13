10efc8680ce778531caf8bf33302150d4d1b8f04
pekeh <pekeh@lenovo.com>
2026-07-13 16:37:43 +0100

    Add agents.py for web search functionality using Langchain and Tavily
    - Introduced agents.py to implement a web search agent utilizing Langchain's ChatOpenAI and TavilySearch.
    - Added functionality to invoke a search for job postings based on user input.
    - Updated pyproject.toml to include langchain-tavily and tavily-python dependencies for enhanced capabilities.
    - Modified uv.lock to reflect new dependencies.

---
+++ agents.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI
+from tavily import TavilyClient
+from langchain_tavily import TavilySearch 
+
+# tavily = TavilyClient()
+# @tool
+# def search(query:str) -> str:
+#     """Search the web for information
+#     Args:
+#         query: The query to search for
+#     Returns:
+#         The search result
+#     """
+#     print(f"Searching for {query}")
+#     return tavily.search(query=query)
+
+llm = ChatOpenAI()
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools)
+
+
+def main():
+
+    print("Hello from agents.py!")
+    result = agent.invoke({"messages": HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the UK on linkedin and list their details")})
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
